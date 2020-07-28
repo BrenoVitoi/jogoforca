ab6b7b66e9808d0b55a62be8fcf859830e4e1a85
Breno Souza <breno.vitoi@gmail.com>
2020-07-28 10:53:52 -0300

    Primeiro commit - carregando arquivos iniciais

---
+++ jogoforca/src/main/java/br/com/softblue/jogoforca/Main.java
@@ -0,0 +1,17 @@
+package br.com.softblue.jogoforca;
+
+import br.com.softblue.jogoforca.game.Game;
+
+public class Main {
+
+	public Main() {
+		
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
+
+	}
+
+}
