78a675d5cd50a2aebae26ab04dae1c43f80d958a
Breno Souza <breno.vitoi@gmail.com>
2020-07-28 16:44:45 -0300

    Imlementando o design pattern Singleton

--- jogoforca/src/main/java/br/com/softblue/jogoforca/core/Dictionary.java
+++ jogoforca/src/main/java/br/com/softblue/jogoforca/core/Dictionary.java
@@ -10,13 +10,24 @@ import br.com.softblue.jogoforca.utils.RandomUtils;
 public class Dictionary {
 
 	private static final String FILE_NAME = "dicionario.txt";
-	
+	private static Dictionary instance;
 	private List<String> words = new ArrayList<>();
+
+	private Dictionary() {
+		load();
+	}
+
+	public static Dictionary getInstance() {
+		if(instance == null) {
+			instance = new Dictionary();
+		}
+		return instance;
+	}
 
 	private void load() throws GameException {
 
 		try (Scanner scanner = new Scanner(getClass().getResourceAsStream("/" + FILE_NAME))) {
-			
+
 			while (scanner.hasNextLine()) {
 				String word = scanner.nextLine().trim();
 				words.add(word);
@@ -27,7 +38,7 @@ public class Dictionary {
 		}
 
 	}
-	
+
 	public Word nextWord() {
 		int pos = RandomUtils.newRandomNumber(0, words.size());
 		return new Word(words.get(pos));
--- jogoforca/src/main/java/br/com/softblue/jogoforca/game/Game.java
+++ jogoforca/src/main/java/br/com/softblue/jogoforca/game/Game.java
@@ -1,17 +1,18 @@
 package br.com.softblue.jogoforca.game;
 
+import br.com.softblue.jogoforca.core.Dictionary;
 import br.com.softblue.jogoforca.core.Word;
 
 public class Game {
 
 	public void start() {
-		Word word = new Word("casa");
-		word.hasChar('a');
-		word.hasChar('c');
-		word.hasChar('s');
-		System.out.println(word.discovered());
 		
-		System.out.println(word);
+		Dictionary d1 = Dictionary.getInstance();
+		System.out.println(d1);
+		
+		Word w1 = d1.nextWord();
+		System.out.println(w1.getOriginalWord());
+		
 	}
 
 }
