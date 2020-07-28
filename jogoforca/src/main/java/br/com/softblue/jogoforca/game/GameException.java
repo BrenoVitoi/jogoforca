8f84b581a6307b369fe5bf8d4ef50275b2f15722
Breno Souza <breno.vitoi@gmail.com>
2020-07-28 16:07:01 -0300

    Criando a classe Dictionary

---
+++ jogoforca/src/main/java/br/com/softblue/jogoforca/game/GameException.java
@@ -0,0 +1,9 @@
+package br.com.softblue.jogoforca.game;
+
+@SuppressWarnings("serial")
+public class GameException extends RuntimeException {
+
+	public GameException(String message) {
+		super(message);
+	}
+}
